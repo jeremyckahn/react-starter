d2c0df2972472e4f20a2ec99503fce7fb52d3d38
Jeremy Kahn <jeremy@jellyvision.com>
2018-07-06 10:29:38 -0500

    Set up ESLint

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { App } from '../src/index';
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import assert from 'assert';
 
